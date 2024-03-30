d9a2c54a7cf3570af00098140138dd41cfec1b6b
Andyko1015 <1155159095@link.cuhk.edu.hk>
2024-03-30 21:02:25 +0800

    提交信息

--- UI design/src/admin/data.js
+++ UI design/src/admin/data.js
@@ -6,27 +6,41 @@ import clothesImage from './img/clothes.jpg';
 export const users = [
   { id: 12345, name: "Alice" },
   { id: 23456, name: "Bob" },
-  { id: 3, name: "Charlie" },
-  { id: 4, name: "Alice" },
-  { id: 2, name: "Bob" },
-  { id: 3, name: "Charlie" }
+  { id: 34567, name: "Charlie" },
+  { id: 45678, name: "Bird" },
+  { id: 56789, name: "Mary" },
+  { id: 67890, name: "Jackson" }
 ];
 
 
 export const products = [
   {
     id: 1,
-    title: 'Electronic 1',
+    title: 'iPhone15 256G',
     price: 9.99,
     imageUrl: phoneImage,
     category: 'Electronics',
   },
   {
     id: 2,
-    title: 'Clothing 1',
+    title: 'Dorothee Schumacher Midi dress',
     price: 19.99,
     imageUrl: clothesImage,
     category: 'Clothing',
+  },
+  {
+    id: 3,
+    title: 'The Black Book of Colors',
+    price: 208,
+    category: 'Clothing',
+    stock: 100,
+  },
+  {
+    id: 4,
+    title: 'Calvin Klein Denim Bucket Hat',
+    price: 490,
+    category: 'Accessories', 
+    stock: 100,
   }
 
 ];
