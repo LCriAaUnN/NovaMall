4c4710989ef1f72c5062965b498c74adc3823adc
Berylmys <73904450+berylmys@users.noreply.github.com>
2024-04-05 11:08:08 +0800

    Update models.py

--- NovaMall/User/models.py
+++ NovaMall/User/models.py
@@ -1,7 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
-class User(models.Model):
-    name = models.CharField(max_length=32)
-    password = models.CharField(max_length=64)
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    stock = models.PositiveIntegerField()
+
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reviews')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews')
+    rating = models.PositiveIntegerField()
+    comment = models.TextField()
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='cart')
+    products = models.ManyToManyField(Product, related_name='carts')
     
