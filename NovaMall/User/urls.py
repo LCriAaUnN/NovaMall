fa71de55ea5440ef56014acf7a3233a16d425a45
Canran Liu <91874288+LCriAaUnN@users.noreply.github.com>
2024-04-11 05:23:15 +0800

    finsh edit function

--- NovaMall/User/urls.py
+++ NovaMall/User/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("list/", views.ListUsersView.as_view(), name="user-list"),
     path("delete/<int:pk>/", views.UserDelete.as_view(), name="user-delete"), 
     path("profile/", views.UserProfileView.as_view(), name="user-profile"),
+    path('edit/', views.EditUserView.as_view(), name='edit_user'),
 ]
--- NovaMall/User/views.py
+++ NovaMall/User/views.py
@@ -2,8 +2,6 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import generics
 from .serializer import UserSerializer
-# from ..ShopPage.models import Product
-# from ..ShopPage.serializer import ProductSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
 
 class CreateUserView(generics.CreateAPIView):
@@ -36,7 +34,9 @@ class EditUserView(generics.UpdateAPIView):
     serializer_class = UserSerializer
     permission_classes = [IsAuthenticated]
 
-
+    def get_object(self):
+        user = self.request.user
+        return User.objects.get(id=user.id)
 
 
 # class ListProductsView(generics.ListAPIView):
