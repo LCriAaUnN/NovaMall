05659a8f152866bdf10c1432c50f65693ec99851
Canran Liu <91874288+LCriAaUnN@users.noreply.github.com>
2024-05-01 22:05:14 +0800

    add comment

--- UI design/src/api.js
+++ UI design/src/api.js
@@ -1,12 +1,19 @@
+// Importing the axios library, which is used to make HTTP requests
 import axios from "axios";
+
+// Importing a constant that holds the key for the access token in local storage
 import { ACCESS_TOKEN } from "./constants";
 
+// The base URL for the API
 const apiUrl = "http://localhost:8000";
 
+// Creating an instance of axios with the base URL set to the API URL
 const api = axios.create({
   baseURL: apiUrl
 });
 
+// Adding an interceptor to the axios instance. This interceptor will run before each request is sent.
+// If there is a token in local storage, it will add it to the Authorization header of the request.
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem(ACCESS_TOKEN);
@@ -15,9 +22,11 @@ api.interceptors.request.use(
     }
     return config;
   },
+  // If there's an error with setting up the request, this function will be called.
   (error) => {
     return Promise.reject(error);
   }
 );
 
+// Exporting the configured axios instance for use in other parts of the application
 export default api;
